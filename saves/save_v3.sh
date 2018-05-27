69c14a097961154199840af644ddef75b3da893a
Devin Gray <dgray@figure.com>
2018-05-27 08:46:44 -0600

    change directory name

--- saves/save_v3.sh
+++ saves/save_v3.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cp -r /Users/devingray/Library/Application\ Support/minecraft/saves/lilly\ and\ dad\ -\ survival\ v3/* ./v3/
+cp -r /Users/devingray/Library/Application\ Support/minecraft/saves/v3/* ./v3/
